d639d42aaff58dc4d1f55dc76ac1e946b01258dd
TomasiakM <marcintomasiak00@gmail.com>
2024-12-10 22:00:19 +0100

    Add GetTotalOrderItemValue tests

---
+++ OrderingApp.Tests/Models/OrderItems/GetTotalOrderItemValue.cs
@@ -0,0 +1,27 @@
+﻿using OrderingApp.Models;
+
+namespace OrderingApp.Tests.Models.OrderItems;
+public class GetTotalOrderItemValue
+{
+    [Fact]
+    public void ShouldReturnValidOrderItemValue()
+    {
+        var product = new Product("name", 5m, 5);
+        var orderItem = new OrderItem(product, 1);
+
+        orderItem.SetDiscount(1m);
+
+        Assert.Equal(4m, orderItem.GetTotalOrderItemValue());
+    }
+
+    [Fact]
+    public void ShouldReturnValidOrderItemValue2()
+    {
+        var product = new Product("name", 5m, 5);
+        var orderItem = new OrderItem(product, 5);
+
+        orderItem.SetDiscount(5m);
+
+        Assert.Equal(20m, orderItem.GetTotalOrderItemValue());
+    }
+}
