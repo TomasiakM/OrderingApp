0fe6a0bf8d15882c6fdf425b242daaf441e47417
TomasiakM <marcintomasiak00@gmail.com>
2024-12-10 21:45:42 +0100

    Update main console app with views

--- OrderingApp/Program.cs
+++ OrderingApp/Program.cs
@@ -1,2 +1,131 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using OrderingApp.Models;
+using OrderingApp.Services.DiscountService;
+
+// { id: { product, quantity }}
+var cart = new Dictionary<int, Tuple<Product, int>>();
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("Wybierz opcję: ");
+
+    Console.WriteLine("1 - Dodaj produkt");
+    Console.WriteLine("2 - Usuń produkt");
+    Console.WriteLine("3 - Zobacz zamówienie");
+    Console.WriteLine("0 - Wyjście");
+
+    var read = Console.ReadLine();
+    switch (read)
+    {
+        case "1":
+            AddProductView();
+            break;
+        case "2":
+            DeleteProductView();
+            break;
+        case "3":
+            ShowOrderView();
+            break;
+        case "0":
+            return;
+    }
+}
+
+void AddProductView()
+{
+    try
+    {
+        Console.Clear();
+
+        Console.WriteLine("Wpisz nazwę nowego produktu:");
+        var name = Console.ReadLine() ?? "";
+
+        Console.WriteLine("Podaj cene produktu:");
+        var price = decimal.Parse(Console.ReadLine() ?? "");
+
+        Console.WriteLine("Podaj ilość produktu w magazynie:");
+        var stock = int.Parse(Console.ReadLine() ?? "");
+
+        Console.WriteLine("Podaj ilość produktu do kupienia:");
+        var quantity = int.Parse(Console.ReadLine() ?? "");
+
+        var id = 1;
+        if (cart.Count > 0)
+        {
+            id = cart.Last().Key + 1;
+        }
+
+        var product = new Product(name, price, stock) { Id = id };
+        cart.Add(id, new(product, quantity));
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Nastąpił błąd podczas tworzenia produktu: " + ex.Message);
+        Console.Read();
+    }
+}
+
+void DeleteProductView()
+{
+    try
+    {
+        Console.Clear();
+
+        Console.WriteLine("Wpisz id produktu do usunięcia:");
+        var id = int.Parse(Console.ReadLine() ?? "");
+
+        if (cart.TryGetValue(id, out var value))
+        {
+            var productDictionary = cart.FirstOrDefault(e => e.Key == id);
+            cart.Remove(productDictionary.Key);
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("Nie znaleziono tego produktu");
+            Console.Read();
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Nastąpił błąd podczas tworzenia produktu: " + ex.Message);
+        Console.Read();
+    }
+}
+
+void ShowOrderView()
+{
+    try
+    {
+        Console.Clear();
+        var orderItems = cart
+            .Select(e => new OrderItem(e.Value.Item1, e.Value.Item2))
+            .ToList();
+
+        var order = new Order(orderItems, new DiscountService());
+
+
+        Console.WriteLine("Twoje zamówienie: ");
+
+        foreach (var item in orderItems)
+        {
+            Console.WriteLine("Id produktu: {0}, Cena: {1}, Ilość: {2}, Obniżka dla tego towaru: {3}",
+                item.ProductId,
+                item.Price,
+                item.Quantity,
+                item.Discount);
+        }
+
+        Console.WriteLine("Ilość przedmiotów w zamówieniu: {0}, Wartość całego zamówienia (po obniżce zamówienia {1}): {2}",
+            order.GetQuantityOfAllProducts(),
+            order.Discount,
+            order.GetOrderValue());
+
+        Console.Read();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Nastąpił błąd podczas tworzenia produktu: " + ex.Message);
+        Console.Read();
+    }
+}
