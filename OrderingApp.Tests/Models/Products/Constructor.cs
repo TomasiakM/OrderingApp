104baddbbc54d7f415d4ca3b1782a801d4e07f53
TomasiakM <marcintomasiak00@gmail.com>
2024-12-10 18:24:54 +0100

    Add product unit tests

---
+++ OrderingApp.Tests/Models/Products/Constructor.cs
@@ -0,0 +1,34 @@
+﻿using OrderingApp.Exceptions;
+using OrderingApp.Models;
+
+namespace OrderingApp.Tests.Models.Products;
+public class Constructor
+{
+    [Fact]
+    public void ShouldCreateValidObject()
+    {
+        var name = "Test";
+        var price = 22.22m;
+        var stock = 5;
+
+        var product = new Product(name, price, stock);
+
+        Assert.Equal(name, product.Name);
+        Assert.Equal(price, product.Price);
+        Assert.Equal(stock, product.Stock);
+    }
+
+    [Theory]
+    [MemberData(nameof(TestCases))]
+    public void ShouldThrowError_WhenAnyPropertyIsInvalid(string name, decimal price, int stock)
+    {
+        Assert.Throws<DomainException>(() => new Product(name, price, stock));
+    }
+
+    public static IEnumerable<object[]> TestCases()
+    {
+        yield return new object[] { "", 2m, 1 };
+        yield return new object[] { "Test", -2m, 1 };
+        yield return new object[] { "Test", 2m, -1 };
+    }
+}
---
+++ OrderingApp.Tests/Models/Products/DecreaseStock.cs
@@ -0,0 +1,34 @@
+﻿using OrderingApp.Exceptions;
+using OrderingApp.Models;
+
+namespace OrderingApp.Tests.Models.Products;
+public class DecreaseStock
+{
+    [Fact]
+    public void DecreaseStock_ShouldDecreaseProductStore()
+    {
+        var product = new Product("1", 20m, 1);
+
+        product.DecreaseStock(1);
+
+        Assert.Equal(0, product.Stock);
+    }
+
+    [Fact]
+    public void DecreaseStock_ShouldThrowException_WhenStockIsNegative()
+    {
+        var product = new Product("1", 20m, 1);
+
+        Assert.Throws<DomainException>(() => product.DecreaseStock(2));
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void DecreaseStock_ShouldThrowException_WhenAmountIsNegative(int amount)
+    {
+        var product = new Product("1", 20m, 1);
+
+        Assert.Throws<DomainException>(() => product.DecreaseStock(amount));
+    }
+}
