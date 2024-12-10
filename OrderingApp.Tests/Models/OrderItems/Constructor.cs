82af1ac5e1b86479181ae01ecaccfa202cbb0cc1
TomasiakM <marcintomasiak00@gmail.com>
2024-12-10 19:11:54 +0100

    Add OrderItem unit tests

---
+++ OrderingApp.Tests/Models/OrderItems/Constructor.cs
@@ -0,0 +1,43 @@
+﻿using OrderingApp.Exceptions;
+using OrderingApp.Models;
+
+namespace OrderingApp.Tests.Models.OrderItems;
+public class Constructor
+{
+    [Fact]
+    public void ShouldCreateValidObject()
+    {
+        var product = new Product("2", 2m, 2) { Id = 5 };
+        var orderItem = new OrderItem(product, 2);
+
+        Assert.Equal(2, orderItem.Quantity);
+        Assert.Equal(product.Price, orderItem.Price);
+        Assert.Equal(product.Id, orderItem.ProductId);
+        Assert.Equal(0m, orderItem.Discount);
+    }
+
+    [Fact]
+    public void ShouldDecreseStockInProduct()
+    {
+        var product = new Product("2", 2m, 2);
+        var orderItem = new OrderItem(product, 2);
+
+        Assert.Equal(0, product.Stock);
+    }
+
+    [Fact]
+    public void ShouldThrowError_WhenProductStockIsLessThanOrderAmount()
+    {
+        var product = new Product("2", 2m, 2);
+
+        Assert.Throws<DomainException>(() => new OrderItem(product, 3));
+    }
+
+    [Fact]
+    public void ShouldThrowError_WhenQuantityIsNegative()
+    {
+        var product = new Product("2", 2m, 2);
+
+        Assert.Throws<DomainException>(() => new OrderItem(product, 0));
+    }
+}
---
+++ OrderingApp.Tests/Models/OrderItems/SetDiscount.cs
@@ -0,0 +1,27 @@
+﻿using OrderingApp.Exceptions;
+using OrderingApp.Models;
+
+namespace OrderingApp.Tests.Models.OrderItems;
+public class SetDiscount
+{
+    [Fact]
+    public void ShouldSetDiscount()
+    {
+        var product = new Product("2", 2m, 2);
+        var orderItem = new OrderItem(product, 2);
+
+        var discount = 1.1m;
+        orderItem.SetDiscount(discount);
+
+        Assert.Equal(discount, orderItem.Discount);
+    }
+
+    [Fact]
+    public void ShouldThrowException_WhenDiscountIsNegative()
+    {
+        var product = new Product("2", 2m, 2);
+        var orderItem = new OrderItem(product, 2);
+
+        Assert.Throws<DomainException>(() => orderItem.SetDiscount(-1));
+    }
+}
